a6e0fdef9ae63bd9a5035d32181327c8f6f4cab3
jesseberliner <jesseberliner@gmail.com>
2019-03-06 09:51:18 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/BookdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookdatabaseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Models/Book.java
@@ -0,0 +1,71 @@
+package com.example.demo.Models;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+
+    private String subtitle;
+
+    private int yearPublished;
+
+    private String description;
+
+    private String genre;
+
+    @ManyToMany
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
